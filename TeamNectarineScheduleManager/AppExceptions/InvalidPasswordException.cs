4993b5546862f2cbead6ca25dc480c98b7fbf1dd
Borislav Borisov <borislavborisov22@abv.bg>
2017-01-08 04:41:27 +0200

    Added Custom exceptions classes for invalid username and password

---
+++ TeamNectarineScheduleManager/AppExceptions/InvalidPasswordException.cs
@@ -0,0 +1,21 @@
+﻿namespace TeamNectarineScheduleManager.AppExceptions
+{
+    using System;
+
+    public class InvalidPasswordException : ApplicationException
+    {
+        public const string defaultMessage =
+            "Invalid password! Password should contain at least 5 digits and one letter";
+
+        public InvalidPasswordException(string message)
+            : base(message)
+        {
+        }
+
+        public InvalidPasswordException()
+            : base(defaultMessage)
+        {
+
+        }
+    }
+}
---
+++ TeamNectarineScheduleManager/AppExceptions/InvalidUsernameException.cs
@@ -0,0 +1,22 @@
+﻿namespace TeamNectarineScheduleManager.AppExceptions
+{
+    using System;
+
+    public class InvalidUsernameException : ApplicationException
+    {
+        public const string defaultMessage =
+            "Invalid username! Username should be at least 5 symbols long and can contain only letters digits or dots or underscores.";
+
+        public InvalidUsernameException(string message)
+            : base(message)
+        {
+
+        }
+
+        public InvalidUsernameException()
+            : base(defaultMessage)
+        {
+
+        }
+    }
+}
