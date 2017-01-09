8d15b6c3a01bd2c9ee3eae9e56db4115e4d8959e
Filip Petkov <Filip.Petkov@gmail.com>
2017-01-09 19:56:22 +0200

    Calendar and DailyEvents Classes remade. Each calendar is an array of list made up of DailyEvents. Events can be added, removed for each day of the year. New OverLoad of ToString method for DailyEvents(needs no paramethers) And Calendar(needs three paramethers)

--- TeamNectarineScheduleManager/Calendars/Calendar.cs
+++ TeamNectarineScheduleManager/Calendars/Calendar.cs
@@ -5,42 +5,48 @@
 
     public class Calendar : DailyEvents
     {
-        private Month _month;
-        private int _week; // specify week number, must be between 1 and 53 inclusive
-        private DayOfWeek _day;
-        public List<DailyEvents> _employeeDailyEvents;//Creates list of with events for the current calendar.
+        public List<DailyEvents>[] personalCalendar;
         private static readonly DateTime[] officialHolidays; // initialize with dates of official holidays
+        public Calendar()
+        {
 
-        public Calendar(Month month, int week, DayOfWeek day)
-        {
-            this._month = month;
-            this._day = day;
-            this._week = week;
-            _employeeDailyEvents = new List<DailyEvents>();
-        }
-        public List<DailyEvents> CalendarEvents
-        {
-            get
+            personalCalendar = new List<DailyEvents>[366];
+
+            List<DailyEvents> daySchedule = new List<DailyEvents>();
+            for (int i = 0; i < 366; i++)
             {
-                return _employeeDailyEvents;
+                personalCalendar[i] = new List<DailyEvents>();
             }
-            set
+            Console.WriteLine(personalCalendar.Count());
+            Console.WriteLine(personalCalendar.ToString());
+        }
+
+        public void AddEvent(int DayOfTheMonth, int Month, int Year, string EventStart, string EventEnd, Events Event)
+        {
+            DailyEvents _event = new DailyEvents(DayOfTheMonth, Month, Year, EventStart, EventEnd, Event);
+            DateTime _eventDate = new DateTime();
+            _eventDate = DateTime.Parse(DayOfTheMonth + "/" + Month + "/" + Year);
+            this.personalCalendar[_eventDate.DayOfYear - 1].Add(_event);
+        }
+
+        public void RemoveEvent(int DayOfTheMonth, int Month, int Year, string EventStart, string EventEnd, int EventNumber)
+        {
+            DateTime _eventDate = new DateTime();
+            _eventDate = DateTime.Parse(DayOfTheMonth + "/" + Month + "/" + Year);
+            this.personalCalendar[_eventDate.DayOfYear - 1].RemoveAt(EventNumber);
+        }
+        public string[] ToString(int DayOfTheMonth, int Month, int Year)
+        {
+            DateTime _eventDate = new DateTime();
+            _eventDate = DateTime.Parse(DayOfTheMonth + "/" + Month + "/" + Year);
+
+            int numberOfEvents = this.personalCalendar[_eventDate.DayOfYear - 1].Count;
+            string[] result = new string[numberOfEvents];
+            for (int i = 0; i < numberOfEvents; i++)
             {
-                _employeeDailyEvents = value;
+                result[i] = "EventNumber[" + i + "] " + this.personalCalendar[_eventDate.DayOfYear - 1][i].ToString();
             }
+            return result;
         }
-        public List<DailyEvents> AddEvent(DailyEvents currentEvent, Calendar calendar)
-        {
-            calendar.CalendarEvents.Add(currentEvent);
-            return CalendarEvents;
-        }
-        // Adds event for the current caledar
-        public List<DailyEvents> RemoveEvent(int removeIndex, Calendar calendar)
-        {
-            calendar.CalendarEvents.RemoveAt(removeIndex);
-            return CalendarEvents;
-        }
-        // Removes event from the current calendar
     }
-
 }
--- TeamNectarineScheduleManager/Calendars/DailyEvents.cs
+++ TeamNectarineScheduleManager/Calendars/DailyEvents.cs
@@ -1,22 +1,52 @@
 ﻿namespace TeamNectarineScheduleManager.Calendars
 {
     using System;
+    using System.Text;
 
     public class DailyEvents
     {
-        private Events _event;
-        private DateTime _eventStart;
-        private DateTime _eventLenght;
+        protected Events _event;
+        protected DateTime _eventStart;
+        protected DateTime _eventEnd;
 
         public DailyEvents()
         {
-
         }
-        public DailyEvents(DateTime eventStart, DateTime eventLenght, Events currentEvent = Events.offDuty)
+        protected internal DailyEvents(int DayOfTheMonth, int Month, int Year, string EventStart = "09:00", string EventEnd = "17:00", Events currentEvent = Events.offDuty)
         {
             this._event = currentEvent;
-            this._eventStart = eventStart;
-            this._eventLenght = eventLenght;
+            if (int.Parse(EventStart.Substring(0, 2)) < 0 | int.Parse(EventStart.Substring(0, 2)) > 23 |
+                int.Parse(EventStart.Substring(3, 2)) < 0 | int.Parse(EventStart.Substring(3, 2)) > 59 |
+                int.Parse(EventEnd.Substring(0, 2)) < 0 | int.Parse(EventEnd.Substring(0, 2)) > 23 |
+                int.Parse(EventEnd.Substring(3, 2)) < 0 | int.Parse(EventEnd.Substring(3, 2)) > 59)
+            {
+                throw new ArgumentOutOfRangeException("Start and end time of an Event must be provided in HH:MM format.");
+            }
+            if (DayOfTheMonth < 1 | DayOfTheMonth > 31)
+            {
+                throw new ArgumentOutOfRangeException("Day of the month for an event must be an integer number ranging from 1 to 31 inclusive");
+            }
+            if (Month < 1 | Month > 12)
+            {
+                throw new ArgumentOutOfRangeException("Month argument of an event must be integer number ranging from 1 to 12 inclusive");
+            }
+            if (Year < 2016 | Year > 2026)
+            {
+                throw new ArgumentOutOfRangeException("Year argument of an event must be integer number in the range of 2016 to 2026 inclusive");
+            }
+
+            this._eventStart = DateTime.Parse(EventStart + " " + DayOfTheMonth + "/" + Month + "/" + Year);
+            this._eventEnd = DateTime.Parse(EventEnd + " " + DayOfTheMonth + "/" + Month + "/" + Year);
+
+        }
+        public override string ToString()
+        {
+            StringBuilder result = new StringBuilder(32);
+            result.Append(this._eventStart.ToString("HH:mm") + " - " + this._eventEnd.ToString("HH:mm"));
+            result.Append(" ");
+            result.Append(this._event);
+            return result.ToString();
         }
     }
 }
+//.name връща типа на евента, начало и край като стринг In traning 10:00 - 14:00
