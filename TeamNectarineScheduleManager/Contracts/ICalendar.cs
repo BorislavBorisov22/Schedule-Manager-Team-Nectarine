1d09821f0ef87fa22ac5b2248a815c087f583638
Borislav Borisov <borislavborisov22@abv.bg>
2017-01-14 17:08:11 +0200

    Added interfaces to some of the classes

---
+++ TeamNectarineScheduleManager/Contracts/ICalendar.cs
@@ -0,0 +1,11 @@
+﻿namespace TeamNectarineScheduleManager.Calendars
+{
+    public interface ICalendar
+    {
+        void AddEvent(int dayOfTheMonth, int month, int year, string eventStart, string eventEnd, EventType evt);
+
+        void RemoveEvent(int dayOfTheMonth, int month, int year, string eventStart, string eventEnd, EventType evt);
+
+        string[] ToString(int dayOfTheMonth, int month, int year);
+    }
+}
---
+++ TeamNectarineScheduleManager/Contracts/IRegularWorker.cs
@@ -0,0 +1,13 @@
+﻿namespace TeamNectarineScheduleManager.Users
+{
+    using TeamNectarineScheduleManager.Teams;
+
+    public interface IRegularWorker
+    {
+        string TeamInfo { get; }
+
+        void JoinTeam(ITeam team);
+
+        void QuitTeam();
+    }
+}
